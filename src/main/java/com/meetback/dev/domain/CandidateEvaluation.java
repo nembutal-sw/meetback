cd39a4a85a8190285511b09eed160cfe453c2644
이래원 <addicto1122@gmail.com>
2026-08-21 15:38:12 +0900

    fix: 형식변환

--- src/main/java/com/meetback/dev/domain/CandidateEvaluation.java
+++ src/main/java/com/meetback/dev/domain/CandidateEvaluation.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 @Data
 public class CandidateEvaluation {
-
     private Long evaluationId;
     private Long candidateId;
     private Integer calculationVersion;
--- src/main/java/com/meetback/dev/domain/CandidateReturnResult.java
+++ src/main/java/com/meetback/dev/domain/CandidateReturnResult.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 @Data
 public class CandidateReturnResult {
-
     private Long resultId;
     private Long meetingId;
     private Long candidateId;
