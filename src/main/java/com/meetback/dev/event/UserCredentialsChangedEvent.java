822bdaf305315ae08e090420c56425efc3b92ac2
Bae Seong Wook <seong4749@gmail.com>
2026-08-27 11:21:11 +0900

    revert: b7c2703 기준으로 변경사항 롤백

--- src/main/java/com/meetback/dev/event/UserCredentialsChangedEvent.java
+++
@@ -1,5 +0,0 @@
-package com.meetback.dev.event;
-
-/** 로그인 자격 정보가 변경된 사용자 세션 종료 이벤트. */
-public record UserCredentialsChangedEvent(Long userId) {
-}
--- src/main/java/com/meetback/dev/event/UserSuspendedEvent.java
+++
@@ -1,6 +0,0 @@
-package com.meetback.dev.event;
-
-public record UserSuspendedEvent(
-        Long userId
-) {
-}
